20d5543ca26074974692b67c4febe770991c8c86
rushabh9430 <sutariyarushabh2@gmail.com>
2023-03-16 11:49:40 +0530

    Initial commit

---
+++ app/src/main/java/com/social/media/application_link/MainActivity.java
@@ -0,0 +1,14 @@
+package com.social.media.application_link;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
